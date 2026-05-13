1df7c5c144cd353823b05b90b4d45df380476310
rnazmo <rnazmo@gmail.com>
2026-05-14 02:25:19 +0900

    chore: format with `npm run format`

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,37 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          {/* <img src={heroImg} className="base" width="170" height="179" alt="" /> */}
-          {/* <img src={reactLogo} className="framework" alt="React logo" /> */}
-          {/* <img src={viteLogo} className="vite" alt="Vite logo" /> */}
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+	return (
+		<>
+			<section id="center">
+				<div className="hero">
+					{/* <img src={heroImg} className="base" width="170" height="179" alt="" /> */}
+					{/* <img src={reactLogo} className="framework" alt="React logo" /> */}
+					{/* <img src={viteLogo} className="vite" alt="Vite logo" /> */}
+				</div>
+				<div>
+					<h1>Get started</h1>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+					</p>
+				</div>
+				<button
+					type="button"
+					className="counter"
+					onClick={() => setCount((count) => count + 1)}
+				>
+					Count is {count}
+				</button>
+			</section>
 
-      <div className="ticks"></div>
+			<div className="ticks"></div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  );
+			<div className="ticks"></div>
+			<section id="spacer"></section>
+		</>
+	);
 }
 
 export default App;
